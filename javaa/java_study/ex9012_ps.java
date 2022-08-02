96b5555ff7deee995d34cdbb4e987eaf0659ebaf
ringring395 <107524209+ringring395@users.noreply.github.com>
2022-08-02 15:07:53 +0900

    8/2

--- javaa/java_study/ex9012_ps.java
+++ javaa/java_study/ex9012_ps.java
@@ -9,29 +9,26 @@ public class ex9012_ps {
 		Scanner s = new Scanner(System.in);
 		Stack st = new Stack();	//스택 사용
 		int N = s.nextInt();	//총 몇번
-
+		Stack print = new Stack();	//출력용 스택
+		
 		for(int i=0; i<N; i++) {
 			String ps = s.next();	//스택에 괄호 입력
 			for(int j=0; j<ps.length(); j++) {
 				if(ps.charAt(j)=='(') {
 					st.push('(');	//여는 괄호면 push
-					
 				}else if(ps.charAt(j)==')') {
 					st.pop();		//닫는 괄호면 pop
 				}
-			}//for 닫음
-
 				if(st.isEmpty()) {
 					//스택 짝이 다 맞아서 비었으면 yes
-					System.out.println("yes");
+					print.push("yes");
 				}else if(!st.isEmpty()) {
 					//스택 짝 안맞으면 no
-					System.out.println("no");;
-				}
-	
+					print.push("no");
+				}					
+			}//for 닫음
+			System.out.println(print.pop());
 		}//for닫음
-
-
 	}//메인메소드 닫음
 
 }
