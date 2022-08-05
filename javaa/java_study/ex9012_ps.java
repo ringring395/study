27db0b0443cfe3cc6dd1b4e0e6e1df73e9eda066
ringring395 <107524209+ringring395@users.noreply.github.com>
2022-08-05 16:49:05 +0900

    Add files via upload

--- javaa/java_study/ex9012_ps.java
+++ javaa/java_study/ex9012_ps.java
@@ -7,26 +7,28 @@ public class ex9012_ps {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		Stack st = new Stack(); // 스택 사용
 		int N = s.nextInt(); // 총 몇번
-		String ps = s.next(); // 스택에 괄호 입력
+		String[] ps = new String[N]; // 괄호를 입력받는 배열(길이는 N)
+		Stack st = new Stack(); // 스택 사용 선언
 
-			for (int j = 0; j < ps.length(); j++) {					
-				if (ps.charAt(j) == '(') {
-					st.push('('); // 여는 괄호면 push
-				} else if (ps.charAt(j) == ')') {
+		for (int i = 0; i < N; i++) { // 입력한 수만큼 아래 for문 돌림
+			ps[i] = s.next(); // 배열에 괄호를 입력받음
+
+			for (int j = 0; j < ps[i].length(); j++) {// 괄호를 하나씩 분석
+
+				if (ps[i].charAt(j) == '(') { // 여는괄호인가요?
+					st.push(3); // 여는 괄호면 push
+				} else if (ps[i].charAt(0) == ')') {
+					st.push(333);
+				} else {
 					st.pop(); // 닫는 괄호면 pop
 				}
-				if (st.isEmpty()) {
-					// 스택 짝이 다 맞아서 비었으면 yes
-					//print.push("yes");
-					System.out.println( "yes");
-				} else if (!st.isEmpty()) {
-					// 스택 짝 안맞으면 no
-					//print.push("no");
-					System.out.println( "no");
-				}
-		} // for닫음
+			} // j_for문 닫음
+		if (st.isEmpty()) { // 스택 짝이 다 맞아서 비었으면 yes
+			System.out.println("yes");
+		}else if (!st.isEmpty()) { // 스택 짝 안맞으면 no
+			System.out.println("no");
+		}
+		} // i_for닫음
 	}// 메인메소드 닫음
-
 }
