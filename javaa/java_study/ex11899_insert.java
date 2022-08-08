83168e6b4e123e8e69f7ee5e30b26b30df6e47fd
ringring395 <107524209+ringring395@users.noreply.github.com>
2022-08-08 15:32:42 +0900

    8/8

--- javaa/java_study/ex11899_insert.java
+++ javaa/java_study/ex11899_insert.java
@@ -7,24 +7,22 @@ public class ex11899_insert {
 //김수진
 	public static void main(String[] args) {
 
-		Scanner s = new Scanner(System.in);
-		String ps = s.next(); // 부러진괄호입력
-		Stack st = new Stack();
-		int result = 0;
-
-		for (int i = 0; i < ps.length(); i++) {
-			if (ps.charAt(i) == '(') {
+		Scanner s = new Scanner(System.in);	
+		Stack st = new Stack();			//스택 사용 선언
+		int count = 0;					//카운트 변수 선언
+		
+		String c = s.next(); // 부러진 괄호입력
+		//입력된 괄호 c 길이만큼 for문 돌림
+		for (int i = 0; i < c.length(); i++) {
+			if (c.charAt(i) == '(') {	//여는 괄호이면 push
 				st.push('(');
-				if (ps.charAt(i + 1) == ')') {
-					st.pop(); // 먼저 넣은 ( pop
-				}
-			} else {
-				st.push(')');
+			}else if(st.empty()) {		//스택에 아무것도 없으면 카운팅
+				count++;
+			}else {						//닫는 괄호이면 pop
+				st.pop();
 			}
-		} // for 닫음
-			//st.elementAt(j).equals('(')) 
-		System.out.println(st);
-		System.out.println(st.size());
+		}//for 닫음		
+		System.out.println(count+st.size());//카운팅이랑 push된 스택길이 더함
 
-	}
+	}//메인메소드 닫음
 }
--- javaa/java_study/ex9012_ps.java
+++ javaa/java_study/ex9012_ps.java
@@ -8,28 +8,30 @@ public class ex9012_ps {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int N = s.nextInt(); // 총 몇번
-		String[] ps = new String[N]; // 괄호를 입력받는 배열(길이는 N)
-		Stack st = new Stack(); // 스택 사용 선언
-		
 
 		for (int i = 0; i < N; i++) { // 입력한 수만큼 아래 for문 돌림
-			ps[i] = s.next(); // 배열에 괄호를 입력받음
-
-			for (int j = 0; j < ps[i].length(); j++) {// 괄호를 하나씩 분석
-
-				if (ps[i].charAt(j) == '(') { // 여는괄호인가요?
-					st.push(3); // 여는 괄호면 push
-				} else if (ps[i].charAt(0) == ')') {
-					st.push(333);
-				} else {
-					st.pop(); // 닫는 괄호면 pop
-				}
-			} // j_for문 닫음
-		if (st.isEmpty()) { // 스택 짝이 다 맞아서 비었으면 yes
-			System.out.println("yes");
-		}else if (!st.isEmpty()) { // 스택 짝 안맞으면 no
-			System.out.println("no");
+			System.out.println(solve(s.next()));
 		}
-		} // i_for닫음
 	}// 메인메소드 닫음
+
+	public static String solve(String ss) {
+		Stack<Character> st = new Stack<>(); // 스택 사용 선언
+		
+		for (int i = 0; i < ss.length(); i++) {// 괄호를 하나씩 분석
+			char c = ss.charAt(i); // 배열에 괄호를 입력받음
+			if (c == '(') { // 여는괄호인가요?
+				st.push(c); // 여는 괄호면 push
+			}else if (st.isEmpty()) {
+				return "no";
+			}else {
+				st.pop();
+			}
+		} // for문 닫음
+
+		if (st.isEmpty()) { // 스택 짝이 다 맞아서 비었으면 yes
+			return "yes";
+		} else { // 스택 짝 안맞으면 no
+			return "no";
+		}
+	} // if닫음
 }
