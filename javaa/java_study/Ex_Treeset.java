ca9ff8a861dc18457da90dd194d08ad98f3f803d
ringring395 <107524209+ringring395@users.noreply.github.com>
2022-08-22 20:34:27 +0900

    8/22 3version

--- javaa/java_study/Ex_Treeset.java
+++ javaa/java_study/Ex_Treeset.java
@@ -14,23 +14,23 @@ public class Ex_Treeset {
 		
 		int N = s.nextInt();				//카드 N장 
 		int k = s.nextInt();				//K번째로 큰수
-		int result = 0;					//결과값 저장 변수
+		int result = 0;						//결과값 저장 변수
 		
-		for(int i=0; i<N; i++) {			//랜덤카드 N장을 treeset에 넣자
+		for(int i=0; i<N; i++) {				//랜덤카드 N장을 treeset에 넣자
 			ts.add((int)(Math.random()*98)+3);				
 		}
 		//랜덤카드 잘 뽑혔는지 확인용
-		System.out.println(ts);
+//		System.out.println(ts);
 		
-		//treeset의 내용을 배열에 저장
-		Integer[] arr = ts.toArray(new Integer[N]);
+
+		Integer[] arr = ts.toArray(new Integer[N]);	//treeset의 내용을 배열에 저장
 		//배열 제대로 들어갔는지 확인용
 //		System.out.println(Arrays.toString(arr));
 
-		if(k == 0) { 	//k가 입력되지 않으면 (null이어야 되는거 아닌가)
+		if(k == 0) { 						//k가 입력되지 않으면 -1(null이어야 되는거 아닌가)
 			result = -1;
 		}else {
-			for(int j=0; j<k; j++) {
+			for(int j=0; j<k; j++) {		//k번째까지 더하기
 				result += arr[j];
 			}
 		}
