da5bd7a2b26624c73acc81becd692866235d958b
ringring395 <107524209+ringring395@users.noreply.github.com>
2022-08-22 20:32:06 +0900

    8/22

--- javaa/java_study/Ex_Treeset.java
+++ javaa/java_study/Ex_Treeset.java
@@ -1,35 +1,40 @@
 package java_study;
 
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Ex_Treeset {
 	//김수진
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 
-			Scanner s = new Scanner(System.in);
-			TreeSet<Integer> ts = new TreeSet<>();	//treeset 사용선언
-			Iterator it = ts.iterator();
-			
-			int N = s.nextInt();				//카드 N장 
-			int k = s.nextInt();				//K번째로 큰수
-			int result = 0;						//결과값 저장 변수
-			int j=0;
-			
-			for(int i=0; i<N; i++) {			//랜덤카드 N장을 treeset에 넣자
-				ts.add((int)(Math.random()*98)+3);
+		Scanner s = new Scanner(System.in);
+		//treeset 선언 , 내림차순 정렬
+		TreeSet<Integer> ts = new TreeSet<>(Collections.reverseOrder());
+		
+		int N = s.nextInt();				//카드 N장 
+		int k = s.nextInt();				//K번째로 큰수
+		int result = 0;					//결과값 저장 변수
+		
+		for(int i=0; i<N; i++) {			//랜덤카드 N장을 treeset에 넣자
+			ts.add((int)(Math.random()*98)+3);				
+		}
+		//랜덤카드 잘 뽑혔는지 확인용
+		System.out.println(ts);
+		
+		//treeset의 내용을 배열에 저장
+		Integer[] arr = ts.toArray(new Integer[N]);
+		//배열 제대로 들어갔는지 확인용
+//		System.out.println(Arrays.toString(arr));
+
+		if(k == 0) { 	//k가 입력되지 않으면 (null이어야 되는거 아닌가)
+			result = -1;
+		}else {
+			for(int j=0; j<k; j++) {
+				result += arr[j];
 			}
-			System.out.println(ts);
-			//k번만큼
-			if(j<k) {
-				result += (int)(it.next());
-				j++;
-			}
-
-			
-			
-
-		s.close();	
-		}//메인메소드 닫음
+		}
+		System.out.println(result);
+	s.close();	
+	}//메인메소드 닫음
 }
